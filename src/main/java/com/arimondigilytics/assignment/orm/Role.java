77d297ebffeb817db48d303da0f2660f2cf6c6d9
RAJAT BANSAL <bansalrajat17@yahoo.in>
2020-02-14 02:41:17 +0530

    14-02-2020

--- src/main/java/com/arimondigilytics/assignment/orm/Role.java
+++ src/main/java/com/arimondigilytics/assignment/orm/Role.java
@@ -1,9 +1,12 @@
 package com.arimondigilytics.assignment.orm;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -12,10 +15,13 @@ import lombok.Data;
 @Entity
 @Table(name = "ROLE")
 public class Role implements Serializable {
-		@Column(name = "ROLE_ID")
-		private String roleId;
 
-		@Column(name = "ROLE_NAME")
-		private String roleName;
-	}
+	@ManyToMany(mappedBy = "roleSet")
+	private Set<User> userSet = new HashSet<>();
+
+	@Column(name = "ROLE_ID")
+	private String roleId;
+
+	@Column(name = "ROLE_NAME")
+	private String roleName;
 }
--- src/main/java/com/arimondigilytics/assignment/orm/User.java
+++ src/main/java/com/arimondigilytics/assignment/orm/User.java
@@ -1,9 +1,15 @@
 package com.arimondigilytics.assignment.orm;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -12,6 +18,11 @@ import lombok.Data;
 @Entity
 @Table(name = "USER")
 public class User implements Serializable {
+
+	@ManyToMany(cascade = { CascadeType.ALL })
+	@JoinTable(name = "USER_ROLE", joinColumns = { @JoinColumn(name = "USER_ID") }, inverseJoinColumns = {
+			@JoinColumn(name = "ROLE_ID") })
+	private Set<Role> roleSet = new HashSet<>();
 
 	@Column(name = "USER_ID")
 	private String userId;
